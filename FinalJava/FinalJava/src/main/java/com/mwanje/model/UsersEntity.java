224ce57e38cb54bde8e16d3aa4e638136c6403ad
dmwanje <49172727+dmwanje@users.noreply.github.com>
2021-07-10 21:09:22 -0600

    commit final java

---
+++ FinalJava/FinalJava/src/main/java/com/mwanje/model/UsersEntity.java
@@ -0,0 +1,103 @@
+package com.mwanje.model;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "Users", schema = "MyLibrary", catalog = "")
+public class UsersEntity {
+    private int idUsers;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Timestamp created;
+
+    @Id
+    @Column(name = "idUsers")
+    public int getIdUsers() {
+        return idUsers;
+    }
+
+    public void setIdUsers(int idUsers) {
+        this.idUsers = idUsers;
+    }
+
+    @Basic
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Basic
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "created")
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UsersEntity that = (UsersEntity) o;
+
+        if (idUsers != that.idUsers) return false;
+        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        if (created != null ? !created.equals(that.created) : that.created != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idUsers;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (created != null ? created.hashCode() : 0);
+        return result;
+    }
+}
---
+++ FinalJava/FinalJava/target/FinalJava-1.0-SNAPSHOT/WEB-INF/templates/users.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: danielmwanje
+  Date: 7/9/21
+  Time: 8:29 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Library Users</title>
+</head>
+<body>
+<h1>Library Users</h1>
+</body>
+</html>
